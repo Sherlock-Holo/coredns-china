eaed44c2a4fd47a6d9fff1a8d3fe58b6f7f40ea3
Sherlock Holo <sherlockya@gmail.com>
2019-11-15 01:16:31 +0800

    build: add build.sh
    Signed-off-by: Sherlock Holo <sherlockya@gmail.com>

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+git clone --depth 1 https://github.com/coredns/coredns.git
+
+cd coredns
+
+git checkout -b build
+
+go get -v github.com/Sherlock-Holo/coredns-china/pkg/plugin
+
+echo china-list:github.com/Sherlock-Holo/coredns-china/pkg/plugin >> plugin.cfg
+
+go generate coredns.go
+
+go mod tidy
+
+go mod download
+
+make
